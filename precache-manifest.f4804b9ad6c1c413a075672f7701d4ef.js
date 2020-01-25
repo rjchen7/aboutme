3ba3f89d07557659c2e3b88a5839a6a990fced31
rjchen7 <ryanchen.fl@gmail.com>
2020-01-24 19:48:44 -0800

    Updates

---
+++ precache-manifest.f4804b9ad6c1c413a075672f7701d4ef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53fd0fdd878d1b22c6b09102842fa0fc",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "1e5ce852b64280f6f0f0",
+    "url": "/react-gh-pages/static/css/main.2197d6e5.chunk.css"
+  },
+  {
+    "revision": "b327d9cd3e38ae8820e4",
+    "url": "/react-gh-pages/static/js/2.1b467007.chunk.js"
+  },
+  {
+    "revision": "1e5ce852b64280f6f0f0",
+    "url": "/react-gh-pages/static/js/main.a02e1c05.chunk.js"
+  },
+  {
+    "revision": "a020c34b70ebe534ba68",
+    "url": "/react-gh-pages/static/js/runtime-main.a76e99a0.js"
+  },
+  {
+    "revision": "c6cf8d3affc7a164c733b4f9c54ff5cf",
+    "url": "/react-gh-pages/static/media/BerkeleySeal.c6cf8d3a.png"
+  },
+  {
+    "revision": "e7c29633b03f71f12c8347f804e67041",
+    "url": "/react-gh-pages/static/media/China.e7c29633.jpg"
+  },
+  {
+    "revision": "2afb91fa920601d80c9df227227eb236",
+    "url": "/react-gh-pages/static/media/Emily.2afb91fa.png"
+  },
+  {
+    "revision": "d32a9299b3cd300d040aa8fb7ee0e2f3",
+    "url": "/react-gh-pages/static/media/GradPhoto.d32a9299.png"
+  },
+  {
+    "revision": "7d779f8fb5cda5712a3e218ef29b8ebf",
+    "url": "/react-gh-pages/static/media/Jordans.7d779f8f.png"
+  },
+  {
+    "revision": "1adfcb9349cdcc0a5bd4b6cac1e1c0a8",
+    "url": "/react-gh-pages/static/media/PayneBen.1adfcb93.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
